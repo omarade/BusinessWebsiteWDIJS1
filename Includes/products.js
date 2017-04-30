9b8da4bf2ee94d9272d01f11889950d728818b77
Omar Abou Dehn <3omar.a.de@gmail.com>
2017-05-01 01:19:36 +0200

    Added products page, CSS file, Javascript file and images

---
+++ Includes/products.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function(){
+	"use strict";
+	$('.more').click(function(){
+      $('.modal-body').empty();
+  	var title = $(this).parent('a').attr("title");
+  	$('.modal-title').html(title);
+  	$($(this).parents('div').html()).appendTo('.modal-body');
+  	$('#myModal').modal({show:true});
+});
+});
